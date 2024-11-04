34fefdc9b453384f6802f9d0ca241c46799f19f8
Anugerah Ashar <nunuzero10@gmail.com>
2024-11-04 10:44:19 +0800

    adding filament resource for question

---
+++ app/Filament/Resources/QuestionResource.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\QuestionResource\Pages;
+use App\Filament\Resources\QuestionResource\RelationManagers;
+use App\Models\Question;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Toggle;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class QuestionResource extends Resource
+{
+    protected static ?string $model = Question::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                RichEditor::make('question')
+                    ->required()
+                    ->columnSpanFull(),
+                RichEditor::make('explanation')
+                    ->required()
+                    ->columnSpanFull(),
+                Repeater::make('answers')
+                    ->required()
+                    ->columnSpanFull()
+                    ->schema([
+                        RichEditor::make('answer'),
+                        Toggle::make('is_the_right_answer')
+                            ->label('The right answer')
+                            ->onIcon('heroicon-m-check')
+                            ->offIcon('heroicon-m-x-mark')
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('question')
+                    ->html()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('explanation')
+                    ->html()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestions::route('/'),
+            'create' => Pages\CreateQuestion::route('/create'),
+            'edit' => Pages\EditQuestion::route('/{record}/edit'),
+        ];
+    }
+}
