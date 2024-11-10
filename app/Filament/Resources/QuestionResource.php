4e7e3e79c17045d6e3503e1f2ae261c3c7583210
Anugerah Ashar <nunuzero10@gmail.com>
2024-11-10 19:31:25 +0800

    Customing UI for Question Resource

--- app/Filament/Resources/QuestionResource.php
+++ app/Filament/Resources/QuestionResource.php
@@ -2,48 +2,57 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\QuestionResource\Pages;
-use App\Filament\Resources\QuestionResource\RelationManagers;
-use App\Models\Question;
-use App\Tables\Columns\CountingAnswers;
 use Filament\Forms;
+use Filament\Tables;
+use App\Models\Question;
 use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Toggle;
+use Filament\Support\Enums\Alignment;
+use App\Tables\Columns\CountingAnswers;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\RichEditor;
-use Filament\Forms\Components\Toggle;
-use Filament\Resources\Resource;
-use Filament\Tables;
-use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\Actions\Action;
+use App\Filament\Resources\QuestionResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\QuestionResource\RelationManagers;
 
 class QuestionResource extends Resource
 {
     protected static ?string $model = Question::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-queue-list';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
                 RichEditor::make('question')
+                    ->label('Question')
                     ->required()
                     ->columnSpanFull(),
                 RichEditor::make('explanation')
+                    ->label('Explanation')
                     ->required()
                     ->columnSpanFull(),
                 Repeater::make('answers')
+                    ->label('Answer Options')
                     ->required()
                     ->columnSpanFull()
                     ->schema([
-                        RichEditor::make('answer'),
+                        RichEditor::make('answer')
+                            ->label('Answer'),
                         Toggle::make('is_the_right_answer')
-                            ->label('The right answer')
+                            ->label('Correct Answer')
                             ->onIcon('heroicon-m-check')
                             ->offIcon('heroicon-m-x-mark')
                     ])
-                    ->grid(2),
+                    ->grid(2)
+                    ->defaultItems(4)
+                    ->addActionLabel('Add answer options')
+                    ->cloneable(),
             ]);
     }
 
@@ -52,18 +61,23 @@ class QuestionResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\TextColumn::make('question')
+                    ->label('Question')
                     ->html()
                     ->searchable(),
                 Tables\Columns\TextColumn::make('explanation')
+                    ->label('Explanation')
                     ->html()
                     ->searchable(),
-                CountingAnswers::make('answers'),
+                CountingAnswers::make('answers')
+                    ->label('Number of Options')
+                    ->alignment(Alignment::Center),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
